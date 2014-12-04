c88c090e2a537ede6e81b77086288ec7f4db7f7e
ihsanudin <surya.kejawen@gmail.com>
2014-12-04 10:31:01 +0700

    repositorynya ketinggalan

---
+++ Entity/ChartRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of JKN
+ *
+ * (c) Muhamad Surya Iksanudin<surya.kejawen@gmail.com>
+ *
+ * @author : Muhamad Surya Iksanudin
+ **/
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class ChartRepository extends EntityRepository
+{
+}
